486db810e6d044bb576e387c9505fa7176f93ee7
Marcelo Soffredi <msoffredi@gmail.com>
2024-08-13 21:50:20 -0500

    Final cleanup for staging CI/CD

--- variables.tf
+++ variables.tf
@@ -9,16 +9,6 @@ variable "region" {
   description = "The region you want your assets to be created and hosted from"
 }
 
-# variable "deploy_bucket" {
-#   type        = string
-#   description = "The cloud storage deployment bucket for your entire project"
-# }
-
-# variable "deploy_prefix" {
-#   type        = string
-#   description = "For local/dev deployments consider using your name or nickname"
-# }
-
 variable "atlas_org_id" {
   type        = string
   description = "Atlas organization id"
@@ -45,22 +35,6 @@ variable "db_user" {
   type        = string
   description = "DB user"
 }
-
-# variable "db_host" {
-#   type        = string
-#   description = "DB host. Should be like: mongodb+srv://<project_name>.<abc123>.mongodb.net"
-# }
-
-# variable "deploy_env" {
-#   type = string
-#   description = "The environment you're deploying to"
-#   default = "dev"
-
-#   validation {
-#     condition     = contains(["dev", "staging", "prod"], var.deploy_env)
-#     error_message = "Valid values for deploy_env are \"dev\", \"staging\", and \"prod\"."
-#   }
-# }
 
 # # Local deployment only. The main.tfvars file is in the .gitignore on purpose 
 # so your keys will stay on your computer, but I do not recommend using 
